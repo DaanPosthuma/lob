d7093d05ee132e9e8344feda3ff9c1c0b09ed4fe
Daan Posthuma <Daan Posthuma>
2024-05-09 08:49:14 +0100

    first commit

---
+++ conanfile.py
@@ -0,0 +1,21 @@
+from conan import ConanFile
+from conan.tools.cmake import cmake_layout
+
+
+class CompressorRecipe(ConanFile):
+    settings = ("os", "compiler", "build_type", "arch")
+    generators = "CMakeToolchain", "CMakeDeps"
+
+    def requirements(self):
+        #self.requires("rapidjson/cci.20211112")
+        #self.requires("boost/1.80.0")
+        #self.requires("openssl/1.1.1q")
+        pass
+
+    def build_requirements(self):
+        self.tool_requires("cmake/3.29.2")
+        self.test_requires("gtest/1.14.0")
+        
+    def layout(self):
+        cmake_layout(self)
+        
---
+++ lob/tests/LOBTests.cpp
@@ -0,0 +1,33 @@
+﻿#include <gtest/gtest.h>
+#include <lob/lob.h>
+#include <unordered_map>
+
+
+namespace {
+
+  TEST(LOBTests, Trivial) {
+    
+    auto book = lob::LimitOrderBook<4>();
+	
+	book.PlaceLimitOrder(lob::Direction::Sell, 100, 231400);
+	book.PlaceLimitOrder(lob::Direction::Sell, 100, 231400);
+	book.PlaceLimitOrder(lob::Direction::Sell, 50, 231300);
+	book.PlaceLimitOrder(lob::Direction::Sell, 100, 231300);
+	book.PlaceLimitOrder(lob::Direction::Sell, 100, 231200);
+	book.PlaceLimitOrder(lob::Direction::Sell, 50, 231200);
+	book.PlaceLimitOrder(lob::Direction::Sell, 100, 231200);
+
+	book.PlaceLimitOrder(lob::Direction::Buy, 100, 230900);
+	book.PlaceLimitOrder(lob::Direction::Buy, 100, 230900);
+	book.PlaceLimitOrder(lob::Direction::Buy, 50, 230800);
+	book.PlaceLimitOrder(lob::Direction::Buy, 100, 230800);
+	book.PlaceLimitOrder(lob::Direction::Buy, 50, 230800);
+	book.PlaceLimitOrder(lob::Direction::Buy, 100, 230700);
+	book.PlaceLimitOrder(lob::Direction::Buy, 100, 230700);
+
+	book.PlaceMarketOrder(lob::Direction::Sell, 270);
+	book.PlaceMarketOrder(lob::Direction::Buy, 560);
+  }
+
+}
+
