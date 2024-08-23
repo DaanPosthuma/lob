11745699d5985b62f20b417661f12cb27debc0b5
Daan Posthuma <Daan Posthuma>
2024-08-23 11:07:06 +0100

    allow running multiple strategies

--- python/script.py
+++ python/script.py
@@ -6,7 +6,7 @@ import pymd as p
 file = p.MappedFile("../data/01302019.NASDAQ_ITCH50")
 reader = p.BinaryDataReader(file)
 symbols = p.Symbols(reader)
-strategy = p.TestStrategy(100)
+strategies = {symbols.byName('QQQ'): [p.TestStrategy(n) for n in [10, 50, 100, 200, 500]]}
 
 idx_market_start = reader.curr()
 def reset_reader():
@@ -15,11 +15,19 @@ def reset_reader():
 print(f'file: {file}')
 print(f'reader: {reader}')
 print(f'symbols: {symbols}')
-print(f'strategy: {strategy}')
+print(f'strategies: {strategies}')
+
+N = 10000000
 
 ts = time.time()
-p.testStrategy(reader, strategy, symbols.byName('QQQ'), 10000000) # 4294967295
+p.testStrategies(reader, strategies, N)
 te = time.time()
-print(f'took {te-ts} s' )
+
+print(f'Took {te - ts}s' )
+
+for id, strategiesForSymbol in strategies.items():
+    print(f'strategies for symbol {id} ({symbols.byId(id)})')
+    for strategy in strategiesForSymbol:
+        strategy.diagnostics.print()
 
 reset_reader()
